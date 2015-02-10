63926cd12d6f69374fd83904da8841bdf1393e91
Kevin Davis <kevin@builditforhumans.com>
2015-02-10 15:23:59 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "sinatra", github: "sinatra"
+gem "pg"
+
+gem "byebug"
---
+++ application.rb
@@ -0,0 +1,40 @@
+require "rubygems"
+require "bundler"
+require "pg"
+Bundler.require :default, (ENV["RACK_ENV"] || "development").to_sym
+
+class Application < Sinatra::Base
+
+  configure :production, :development do
+    enable :logging
+  end
+
+  def initialize
+    connection_hash = {
+      host: ENV['ANALYSIS_DB_HOST'],
+      dbname: ENV['ANALYSIS_DB_DBNAME'],
+      user: ENV['ANALYSIS_DB_USER'],
+      password: ENV['ANALYSIS_DB_PW']
+    }
+    @db = PG.connect(connection_hash)
+  end
+
+  get "/segment" do
+    begin
+      @db.exec("INSERT INTO events (          \
+                              event_name,     \
+                              occurred_at,    \
+                              user_id,        \
+                              details         \
+                ) VALUES (                    \
+                    #{params['event']},       \
+                    #{params['timestamp']},   \
+                    #{params['userId']},      \
+                    #{params['properties']}   \
+                )")
+    rescue PG::Error => err
+      logger.error "Problem inserting an event (#{params['event']}) at #{params['timestamp']}"
+    end
+  end
+
+end
